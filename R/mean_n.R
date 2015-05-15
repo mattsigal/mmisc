c2a2d1b80b8a86a93206dd1aa1d1ae9f792ad9a3
mattsigal <matthewsigal@gmail.com>
2015-05-15 10:21:39 -0400

    Added mean_n.

---
+++ R/mean_n.R
@@ -0,0 +1,25 @@
+#' SPSS Style Means
+#'
+#' \code{mean_n} returns the row-wise average of a series of vectors. The n
+#' indicates the minimimum number of items a particpant should have responded
+#' to in order to be given a mean.
+#'
+#' @param df An R dataframe object
+#' @param n Minimum number of items with valid responses
+#' @return A vector of type numeric.
+#' @examples
+#' \dontrun{
+#' set.seed(77)
+#' x <- data.frame(matrix(rnorm(n = 50), ncol = 5))
+#' x[1,1:4] <- NA
+#' mean_n(x, n = 2)
+#' }
+#' @seealso \code{\link{scoring}}
+#'
+
+mean_n <- function(df, n) {
+  means <- apply(as.matrix(df), 1, mean, na.rm = TRUE)
+  nvalid <- apply(as.matrix(df), 1, function(df) sum(!is.na(df)))
+  ret <- ifelse(nvalid >= n, means, NA)
+  return(ret)
+}
